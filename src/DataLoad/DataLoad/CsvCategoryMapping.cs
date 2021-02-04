d75d0211a0f94739d6047a56d296fe64597479e7
Chad Green <chadgreen@chadgreen.com>
2021-02-03 23:54:30 -0500

    Added the ability to import categories

---
+++ src/DataLoad/DataLoad/CsvCategoryMapping.cs
@@ -0,0 +1,15 @@
+﻿using TinyCsvParser.Mapping;
+
+namespace DataLoad
+{
+	public class CsvCategoryMapping : CsvMapping<Category>
+	{
+
+		public CsvCategoryMapping()
+		{
+			MapProperty(0, x => x.RebrickableId);
+			MapProperty(1, x => x.Name);
+		}
+
+	}
+}
--- src/DataLoad/DataLoad/Program.cs
+++ src/DataLoad/DataLoad/Program.cs
@@ -19,13 +19,13 @@ namespace DataLoad
 		{
 
 
-			CsvParserOptions csvParserOptions = new CsvParserOptions(true, ',');
-			CsvColorMapping csvMapper = new CsvColorMapping();
-			CsvParser<Color> csvParser = new CsvParser<Color>(csvParserOptions, csvMapper);
+			//CsvParserOptions csvParserOptions = new CsvParserOptions(true, ',');
+			//CsvColorMapping csvMapper = new CsvColorMapping();
+			//CsvParser<Color> csvParser = new CsvParser<Color>(csvParserOptions, csvMapper);
 
-			var result = csvParser
-					.ReadFromFile(@"C:\Users\chadg\Downloads\Lego Database\colors.csv", Encoding.ASCII)
-					.ToList();
+			//var result = csvParser
+			//		.ReadFromFile(@"C:\Users\chadg\Downloads\Lego Database\colors.csv", Encoding.ASCII)
+			//		.ToList();
 
 			string containerLink = $"/dbs/{Settings.Database}/colls/{Settings.Container}";
 			Console.WriteLine($"Connecting to: host: {Settings.Host}, port: {Settings.Port}, container: {containerLink}, ssl: {Settings.EnableSSL}");
@@ -33,7 +33,7 @@ namespace DataLoad
 																							username: containerLink,
 																							password: Settings.PrimaryKey);
 
-			ConnectionPoolSettings connectionPoolSettings = new ConnectionPoolSettings()
+			ConnectionPoolSettings connectionPoolSettings = new()
 			{
 				MaxInProcessPerConnection = 10,
 				PoolSize = 30,
@@ -54,13 +54,16 @@ namespace DataLoad
 					connectionPoolSettings,
 					webSocketConfiguration);
 
-			await gremlinClient.SubmitAsync<dynamic>("g.V().has('userId', 'catalog').hasLabel('color').drop()");
-			foreach (var color in result)
-			{
-				Console.WriteLine($"{color.Result.Name}");
-				string query = $"g.addV('color').property('userId', 'catalog').property('id', 'color_{color.Result.Id}').property('name', '{color.Result.Name}').property('rgb', '{color.Result.RGB}').property('isTranslucent', '{color.Result.IsTranslucent}')";
-				SubmitRequest(gremlinClient, query);
-			}
+			//await gremlinClient.SubmitAsync<dynamic>("g.V().has('userId', 'catalog').hasLabel('color').drop()");
+			//foreach (var color in result)
+			//{
+			//	Console.WriteLine($"{color.Result.Name}");
+			//	string query = $"g.addV('color').property('userId', 'catalog').property('id', 'color_{color.Result.Id}').property('name', '{color.Result.Name}').property('rgb', '{color.Result.RGB}').property('isTranslucent', '{color.Result.IsTranslucent}')";
+			//	SubmitRequest(gremlinClient, query);
+			//}
+
+			await ImportColorsAsync(gremlinClient, @"C:\Users\chadg\Downloads\Lego Database\");
+			await ImportCategoriesAsync(gremlinClient, @"C:\Users\chadg\Downloads\Lego Database\");
 
 		}
 
@@ -81,6 +84,52 @@ namespace DataLoad
 				Console.ForegroundColor = foregroundColor;
 				throw;
 			}
+		}
+
+		private static CsvParserOptions GetCsvParserOptions()
+		{
+			return new CsvParserOptions(true, ',');
+		}
+
+		private static async Task ImportColorsAsync(GremlinClient gremlinClient, string filePath = @"C:\Users\chadg\Downloads\Lego Database\")
+		{
+
+			CsvColorMapping csvMapper = new();
+			CsvParser<Color> csvParser = new(GetCsvParserOptions(), csvMapper);
+
+			var result = csvParser
+					.ReadFromFile(@$"{filePath}colors.csv", Encoding.ASCII)
+					.ToList();
+
+			await gremlinClient.SubmitAsync<dynamic>("g.V().has('userId', 'catalog').hasLabel('color').drop()");
+			foreach (var color in result)
+			{
+				Console.WriteLine($"{color.Result.Name}");
+				string query = $"g.addV('color').property('userId', 'catalog').property('id', 'color_{color.Result.Id}').property('name', '{color.Result.Name}').property('rgb', '{color.Result.RGB}').property('isTranslucent', '{color.Result.IsTranslucent}')";
+				SubmitRequest(gremlinClient, query);
+			}
+
+		}
+
+
+		private static async Task ImportCategoriesAsync(GremlinClient gremlinClient, string filePath = @"C:\Users\chadg\Downloads\Lego Database\")
+		{
+
+			CsvCategoryMapping csvMapper = new();
+			CsvParser<Category> csvParser = new(GetCsvParserOptions(), csvMapper);
+
+			var result = csvParser
+					.ReadFromFile(@$"{filePath}part_categories.csv", Encoding.ASCII)
+					.ToList();
+
+			await gremlinClient.SubmitAsync<dynamic>("g.V().has('userId', 'catalog').hasLabel('category').drop()");
+			foreach (var category in result)
+			{
+				Console.WriteLine($"{category.Result.Name}");
+				string query = $"g.addV('category').property('userId', 'catalog').property('id', 'category_{category.Result.RebrickableId}').property('name', '{category.Result.Name}')";
+				SubmitRequest(gremlinClient, query);
+			}
+
 		}
 
 	}
